c4eb194718ed8a5cd0405b22a7c69d74801c3007
MDRahul47e <rahulahames@gmail.comm>
2022-03-26 11:50:13 +0600

    add

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Games For You</h1>
+            <h2>Choose 4 Games</h2>
+        </div>
+    );
+};
+
+export default Header;
